e62fcdfe3607837a81dff9c85623cac3d72ccc26
whatsgit <30848040+whatsgit@users.noreply.github.com>
2018-03-19 10:38:37 -0500

    Update simpcalc.py

--- simpcalc.py
+++ simpcalc.py
@@ -10,7 +10,10 @@ while True:
    if user_input == "quit":
       break
    elif user_input == "add":
-      ...
+      num1 = float(input("Enter a number: "))
+      num2 = float(input("Enter another number: "))
+      result = str(num1 + num2)
+      print("The answer is " + result)
    elif user_input == "subtract":
       ...
    elif user_input == "multiply":
