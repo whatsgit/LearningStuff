ad88e3a1202cb75a9ca8674b0f1e7d9889d65455
whatsgit <30848040+whatsgit@users.noreply.github.com>
2018-03-19 10:30:53 -0500

    Update play.py

--- play.py
+++ play.py
@@ -10,3 +10,30 @@ nums = list(range(5, 8))
 print(len(nums))
 
 print(list (range(8,3)))
+
+numbers = list(range(5, 20, 2))
+print(numbers)
+#>>>
+#[5, 7, 9, 11, 13, 15, 17, 19]
+#>>>
+
+words = ["hello", "world", "spam", "eggs"]
+counter = 0 
+max_index = len(words) - 1   ##This sets teh threshold for the counter to check against number of words minus one for the last item in the index
+
+while counter <= max_index:
+   word = words[counter]  ##prints the item at the index referenced by counter variable
+   print(word + "!")
+   counter = counter + 1  ##increments the counter up one index value.
+
+##the for construct will run the iteration until it has hit the last item in the index without the need of a counter##
+words = ["hello", "world", "spam", "eggs"]
+for word in words:  ##for each word in the list of words do the following##
+  print(word + "!")
+  
+  for i in range(5):
+  print("hello!")
+  
+  
+ for i in range(0, 20, 2):
+  print(i)
