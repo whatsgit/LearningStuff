d952e107cafadad7cd64c2f49ff90b943aa4731d
whatsgit <30848040+whatsgit@users.noreply.github.com>
2018-04-02 16:06:18 -0500

    Update play.py

--- play.py
+++ play.py
@@ -581,3 +581,36 @@ At least one is even
 (4, 11)
 >>>
 
+filename = input("Enter a filename: ")
+
+with open(filename) as f:
+   text = f.read()
+
+print(text)
+
+
+##This part of the program shows a function that counts how many times a character occurs in a string. def count_char(text, char):
+  count = 0
+  for c in text:
+    if c == char:
+      count += 1
+  return count
+##This function takes as its arguments the text of the file and one character, returning the number of times that character appears in the text.
+##Now we can call it for our file. filename = input("Enter a filename: ")
+with open(filename) as f:
+  text = f.read()
+
+print(count_char(text, "r"))
+>>>
+Enter a filename: test.txt
+83
+>>>
+
+##This is an example project, showing a program that analyzes a sample file to find what percentage of the text each character occupies.
+##This section shows how a file could be open and read. filename = input("Enter a filename: ")
+
+with open(filename) as f:
+   text = f.read()
+
+print(text)
+
